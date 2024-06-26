f87d4d48922f5f88467bc991baac3141ad633e3b
Talleux <ewen.talleux@epitech.eu>
2024-06-26 16:14:40 +0200

    feat(setup audio backend): initial commit with build script

---
+++ audiobackend/build.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/thestk/rtaudio.git
+cd rtaudio
+mkdir build
+cd build
+cmake ..
+make
+sudo cmake --install .
+cd ../..
+mkdir build
+cd build
+cmake ..
+make
+mv showtime-audio-backend ..
+cd ..
