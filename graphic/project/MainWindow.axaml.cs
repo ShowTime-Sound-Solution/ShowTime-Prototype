e30eb926c71d7ea467e361e73ea9959b62a25fdc
Bastien Mazzolini <bastien.mazzolini@tutteo.com>
2024-06-28 10:12:52 +0200

    [+] front

---
+++ graphic/project/MainWindow.axaml.cs
@@ -0,0 +1,39 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
+namespace project;
+
+public partial class MainWindow : Window
+{
+    private bool _isFullScreen = false;
+    public MainWindow()
+    {
+        InitializeComponent();
+        this.KeyDown += MainWindow_KeyDown;
+    }
+    
+    private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.F11)
+        {
+            ToggleFullScreen();
+        }
+    }
+
+    private void ToggleFullScreen()
+    {
+        if (_isFullScreen)
+        {
+            this.WindowState = WindowState.Normal;
+            this.Width = 1920;
+            this.Height = 1080;
+            _isFullScreen = false;
+        }
+        else
+        {
+            this.WindowState = WindowState.FullScreen;
+            _isFullScreen = true;
+        }
+    }
+}
