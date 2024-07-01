cdcb0ce17320881bd598c7b27701c1fc70f51cea
Benjamin Goussard <benjamin.goussard@epitech.eu>
2024-07-01 17:17:54 +0200

    feat(effect): add a reverb with 3 buffers

--- audiobackend/src/Effects/ReverbEffect.hpp
+++ audiobackend/src/Effects/ReverbEffect.hpp
@@ -16,22 +16,32 @@ public:
     }
 
     void process(float *inputBuffer, float *outputBuffer, unsigned int nBufferFrames) override {
-        if (impulseResponse == nullptr) {
-            impulseResponse = new float[nBufferFrames];
+        if (!isEnabled()) {
+            for (unsigned int i = 0; i < nBufferFrames; i++) {
+                outputBuffer[i] = inputBuffer[i];
+            }
+            return;
+        }
+        if (count < 3) {
             for (int i = 0; i < nBufferFrames; i++) {
                 outputBuffer[i] = inputBuffer[i];
             }
+            count++;
         } else {
             for (int i = 0; i < nBufferFrames; i++) {
-                outputBuffer[i] = inputBuffer[i] + impulseResponse[i] * 0.5f;
+                outputBuffer[i] = inputBuffer[i] * 0.8 + impulseResponse3[i] * 0.4f;
             }
         }
         for (int i = 0; i < nBufferFrames; i++) {
-            impulseResponse[i] = outputBuffer[i];
+            impulseResponse3[i] = impulseResponse2[i];
+            impulseResponse2[i] = impulseResponse1[i];
+            impulseResponse1[i] = outputBuffer[i];
         }
     }
 
-
 private:
-    float *impulseResponse = nullptr;
+    float *impulseResponse1 = new float[512 * 2];
+    float *impulseResponse2 = new float[512 * 2];
+    float *impulseResponse3 = new float[512 * 2];
+    int count = 0;
 };
