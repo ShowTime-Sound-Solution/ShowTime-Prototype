9a36ca80378a2cb2a6a239105fb17139b6ddafab
Talleux <ewen.talleux@epitech.eu>
2024-07-02 11:50:31 +0200

    feat(audiobackend api): adding more reverb

--- audiobackend/src/Effects/ReverbEffect.hpp
+++ audiobackend/src/Effects/ReverbEffect.hpp
@@ -15,6 +15,17 @@ public:
         setId(id);
     }
 
+    ~ReverbEffect() {
+        delete[] impulseResponse1;
+        delete[] impulseResponse2;
+        delete[] impulseResponse3;
+        delete[] impulseResponse4;
+        delete[] impulseResponse5;
+        delete[] impulseResponse6;
+        delete[] impulseResponse7;
+        delete[] impulseResponse8;
+    }
+
     void process(float *inputBuffer, float *outputBuffer, unsigned int nBufferFrames) override {
         if (!isEnabled()) {
             for (unsigned int i = 0; i < nBufferFrames; i++) {
@@ -22,17 +33,22 @@ public:
             }
             return;
         }
-        if (count < 3) {
+        if (count < 8) {
             for (int i = 0; i < nBufferFrames; i++) {
                 outputBuffer[i] = inputBuffer[i];
             }
             count++;
         } else {
             for (int i = 0; i < nBufferFrames; i++) {
-                outputBuffer[i] = inputBuffer[i] * 0.8 + impulseResponse3[i] * 0.4f;
+                outputBuffer[i] = inputBuffer[i] * 0.9 + impulseResponse8[i] * 0.5f;
             }
         }
         for (int i = 0; i < nBufferFrames; i++) {
+            impulseResponse8[i] = impulseResponse7[i];
+            impulseResponse7[i] = impulseResponse6[i];
+            impulseResponse6[i] = impulseResponse5[i];
+            impulseResponse5[i] = impulseResponse4[i];
+            impulseResponse4[i] = impulseResponse3[i];
             impulseResponse3[i] = impulseResponse2[i];
             impulseResponse2[i] = impulseResponse1[i];
             impulseResponse1[i] = outputBuffer[i];
@@ -43,5 +59,10 @@ private:
     float *impulseResponse1 = new float[512 * 2];
     float *impulseResponse2 = new float[512 * 2];
     float *impulseResponse3 = new float[512 * 2];
+    float *impulseResponse4 = new float[512 * 2];
+    float *impulseResponse5 = new float[512 * 2];
+    float *impulseResponse6 = new float[512 * 2];
+    float *impulseResponse7 = new float[512 * 2];
+    float *impulseResponse8 = new float[512 * 2];
     int count = 0;
 };
