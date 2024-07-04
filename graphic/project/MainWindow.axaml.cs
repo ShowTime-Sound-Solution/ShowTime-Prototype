e0d309179b6fa076820a7680cfde65f33289649b
PaulinLec <paulin.leclercq@epitech.eu>
2024-07-04 23:10:31 +0200

    feat(propagation): displaying sum of both audio sources

--- graphic/project/MainWindow.axaml.cs
+++ graphic/project/MainWindow.axaml.cs
@@ -66,7 +66,8 @@ public partial class MainWindow : Window
         // Add walls in the room
         SelectLayout(1);
 
-        AddAudioSource(RoomHeight / 2, RoomWidth / 3 * 2, 0);
+        AddAudioSource(RoomHeight / 3 * 2, RoomWidth / 3 * 2, 0);
+        AddAudioSource(RoomHeight / 3 * 1, RoomWidth / 3 * 2, 0);
         DrawRoom();
     }
 
@@ -95,6 +96,7 @@ public partial class MainWindow : Window
                     _room[RoomHeight / 4 + i, RoomWidth / 4 * 3] = new Box { Type = BoxType.Wall };
                 break;
         }
+
         DrawRoom();
     }
 
@@ -102,53 +104,65 @@ public partial class MainWindow : Window
     {
         if (buffer.Length < 4)
             return;
-
-        var floats = new float[512];
+        var floatsLeft = new float[512 / 2];
+        var floatsRight = new float[512 / 2];
         for (var i = 0; i < 512; i++)
-            floats[i] = BitConverter.ToSingle(buffer, i * 4);
+        {
+            if (i % 2 == 0)
+                floatsLeft[i / 2] = BitConverter.ToSingle(buffer, i * 4);
+            else
+                floatsRight[i / 2] = BitConverter.ToSingle(buffer, i * 4);
+        }
 
-        var decibels = floats.Max(Math.Abs) * 300;
-        if (decibels > 300)
+        var decibelsLeft = floatsLeft.Max(x => Math.Abs(x)) * 100;
+        var decibelsRight = floatsRight.Max(x => Math.Abs(x)) * 100;
+        if (decibelsLeft > 300 || decibelsRight > 300)
             return;
 
-        _buffers.Enqueue(decibels);
-        if (_buffers.Count > 10)
-            _buffers.Dequeue();
-        foreach (var box in _room)
-            box.Decibels = 0;
-        foreach (var source in _audioSources)
-            _room[source.Item1, source.Item2].Decibels = _buffers.Average();
+        _buffersLeft.Enqueue(decibelsLeft);
+        _buffersRight.Enqueue(decibelsRight);
+        if (_buffersLeft.Count > 5)
+            _buffersLeft.Dequeue();
+        if (_buffersRight.Count > 5)
+            _buffersRight.Dequeue();
 
+        foreach (var box in _room)
+            box.Decibels.Clear();
+        _room[_audioSources[0].Item1, _audioSources[0].Item2].Decibels[0] = _buffersRight.Average();
+        _room[_audioSources[1].Item1, _audioSources[1].Item2].Decibels[1] = _buffersLeft.Average();
+
+        // var now = DateTime.Now;
         PropagateSound();
+        // Console.WriteLine((DateTime.Now - now).TotalMilliseconds);
         DrawRoom();
     }
 
     private void AddAudioSource(int x, int y, double decibels = 100)
     {
-        _room[x, y].Decibels = decibels;
-        _room[x, y].Type = BoxType.Source;
         _audioSources.Add(new Tuple<int, int>(x, y));
+        _room[x, y].Decibels[_audioSources.Count - 1] = decibels;
+        _room[x, y].Type = BoxType.Source;
     }
 
     private void PropagateSound()
     {
         Array.Copy(_room, _newRoom, _room.Length);
 
-        foreach (var (x, y) in _audioSources)
-            PropagateSoundFromSource(x, y, _newRoom);
+        for (var i = 0; i < _audioSources.Count; i++)
+            PropagateSoundFromSource(i, _newRoom);
 
         Array.Copy(_newRoom, _room, _room.Length);
     }
 
-    private void PropagateSoundFromSource(int sourceX, int sourceY, Box[,] newRoom)
+    private void PropagateSoundFromSource(int sourceIndex, Box[,] newRoom)
     {
         var queue = new Queue<Tuple<int, int>>();
-        queue.Enqueue(new Tuple<int, int>(sourceX, sourceY));
+        queue.Enqueue(new Tuple<int, int>(_audioSources[sourceIndex].Item1, _audioSources[sourceIndex].Item2));
 
         while (queue.Count > 0)
         {
             var (x, y) = queue.Dequeue();
-            var decibels = newRoom[x, y].Decibels;
+            var decibels = newRoom[x, y].Decibels[sourceIndex];
             if (decibels < 0.01)
                 continue;
 
@@ -159,17 +173,16 @@ public partial class MainWindow : Window
                     continue;
 
                 var distance = Math.Sqrt((x - nx) * (x - nx) + (y - ny) * (y - ny));
-                var newVolume = decibels - distance;
-                if (newVolume < 0 || newVolume <= newRoom[nx, ny].Decibels)
+                var newVolume = decibels - distance * 1.2;
+                if (newVolume < 0 ||
+                    (newRoom[nx, ny].Decibels.TryGetValue(sourceIndex, out var value) && newVolume <= value))
                     continue;
 
-                newRoom[nx, ny].Decibels = newVolume;
+                newRoom[nx, ny].Decibels[sourceIndex] = newVolume;
                 queue.Enqueue(new Tuple<int, int>(nx, ny));
 
-                if (newVolume > _maxDecibels)
-                    _maxDecibels = newVolume;
-                if (newVolume != 0 && newVolume < _minDecibels)
-                    _minDecibels = newVolume;
+                _maxDecibels = double.Max(_maxDecibels, newVolume);
+                _minDecibels = double.Min(_minDecibels, newVolume);
             }
         }
     }
@@ -186,7 +199,7 @@ public partial class MainWindow : Window
             var color = -1;
             if (_room[i, j].Type == BoxType.Air)
             {
-                var normalizedDecibels = (_room[i, j].Decibels - _minDecibels) / decibelRange;
+                var normalizedDecibels = (_room[i, j].Decibels.Sum(x => x.Value) - _minDecibels) / decibelRange;
                 normalizedDecibels = Math.Max(0, Math.Min(normalizedDecibels, 1));
                 color = (int)(normalizedDecibels * 255);
             }
@@ -227,7 +240,7 @@ public partial class MainWindow : Window
 
         this.Find<Image>("SimulationImage")!.Source = bitmap;
     }
-    
+
     public static readonly Client.Client Client = new();
     private bool _isFullScreen;
 
@@ -241,16 +254,17 @@ public partial class MainWindow : Window
     private readonly List<Tuple<int, int>> _audioSources = [];
 
     private readonly byte[] _pixels = new byte[RoomSize * RoomSize * 4];
-    private readonly Queue<float> _buffers = [];
+    private readonly Queue<float> _buffersLeft = [];
+    private readonly Queue<float> _buffersRight = [];
 
     private double _maxDecibels = 1;
     private double _minDecibels;
 
-    private void ChangeScene(object? sender, RoutedEventArgs e)
+    private void ChangeScene(object? sender, RoutedEventArgs _)
     {
-        var button = ( sender as Button )!;
+        var button = (sender as Button)!;
         var value = int.Parse(button.Name![6].ToString());
-        
+
         SelectLayout(value);
     }
 }
@@ -258,7 +272,7 @@ public partial class MainWindow : Window
 public class Box
 {
     public BoxType Type { get; set; } = BoxType.Air;
-    public double Decibels { get; set; }
+    public Dictionary<int, double> Decibels { get; } = [];
 }
 
 public enum BoxType
